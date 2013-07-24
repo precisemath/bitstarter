72aedbb18947428f116a4891a8759500e870a21c
precisemath <pknaren@gmail.com>
2013-07-23 22:46:21 -0700

    Finally done with index.html

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs      = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest    = require('restler');
+var sys     = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var TEMP_FILE = "./temp.tmp";
 
 var assertFileExists = function(infile) {
 	var instr = infile.toString();
@@ -36,8 +39,16 @@ var assertFileExists = function(infile) {
 	return instr;
 };
 
+var assertURLExists = function(url) {
+  return url; 
+};
+
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var cheerioURLData = function(urlData) {
+    return cheerio.load(urlData);
 };
 
 var loadChecks = function(checksfile) {
@@ -46,6 +57,17 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var checkURL = function(urlData, checksfile) {
+    $ = cheerioURLData(urlData);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -64,11 +86,41 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists) )
+        .option('-u, --url  <URL>', 'Any URL', clone(assertURLExists) )
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    //Check for the parameters
+    if (!program.file && !program.url) {
+       console.log("Need to use either parameter --file or --url. Exiting.");
+       process.exit(1); 
+    }
+
+    if (program.file && program.url) {
+       console.log("Use either --file or --url, Not both. Exiting.");
+       process.exit(1); 
+    }
+
+    var checkJson;
+    console.log ("%s",program.url);
+
+    if (program.file) {
+       checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    else {
+      rest.get(program.url).on('complete', function(result,response) {
+         if (result instanceof Error) {
+            console.log("%s does not exist.Exiting.",url);
+            process.exit(1);
+         }
+         fs.writeFileSync(TEMP_FILE, result);
+         return response;
+      });
+       checkJson = checkHtmlFile(TEMP_FILE, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+    exports.checkcheckURL = checkURL;
 }
