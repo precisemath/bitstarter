bef105213ed2ed2fa1b11831759affe0f30d5c1d
precisemath <pknaren@gmail.com>
2013-08-10 17:26:26 -0700

    Changed port Number

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
 });
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
